cc95014569bf5c3158e3e2b5238bc1d4ac294167
ftlvz <francesco.tartaggia+github@lavazza.com>
2025-03-25 16:10:25 +0100

    Create sonar_ignore_this.py

---
+++ src/bromine/webdriver/sonar_ignore_this.py
@@ -0,0 +1,62 @@
+from .window import Window
+
+# --- create duplicated code for sonar ---
+class ResizableWindow(Window):
+
+    @Window.outer_size.setter
+    def outer_size(self, value):
+        self._browser.set_window_size(*value)
+        assert_that(self.outer_size, equal_to(value), 'Outer Window Size')
+
+    @Window.inner_size.setter
+    def inner_size(self, value):
+        size_adjustment = value - self.inner_size
+        try:
+            self.outer_size += size_adjustment
+        except AssertionError:
+            pass
+        assert_that(self.inner_size, equal_to(value), 'Inner Window Size')
+
+    def maximize(self):
+        self._browser.maximize_window()
+
+    def __init__(self, browser):
+        self._browser = browser
+
+    @property
+    @RectSize.wrapped
+    def outer_size(self):
+        size = self._browser.get_window_size()
+        return (size['width'], size['height'])
+
+    @property
+    @RectSize.wrapped
+    def inner_size(self):
+        return self._browser.execute_script(
+            'return [window.innerWidth, window.innerHeight];')
+
+    @property
+    def virtual_pixel_ratio(self):
+        return self._browser.execute_script('return window.devicePixelRatio;')
+
+    @property
+    def physical_pixel_ratio(self):
+        return self.virtual_pixel_ratio
+
+    @property
+    def address_bar_height(self):
+        if hasattr(self._browser, 'address_bar_height'):
+            return self._browser.address_bar_height
+        else:
+            return 0
+
+    @property
+    def bar_shadow_height(self):
+        if hasattr(self._browser, 'bar_shadow_height'):
+            return self._browser.bar_shadow_height
+        else:
+            return 0
+
+    def get_screenshot(self):
+        png_bytes = self._browser.get_screenshot_as_png()
+        return ScreenshotFromPngBytes(png_bytes)
