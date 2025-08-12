4b455ed67a9d2f62388295ba668f94c0cc07a323
ftlvz <francesco.tartaggia+github@lavazza.com>
2025-08-12 15:45:57 +0200

    Create sonar_experiment_parsing_error.py

---
+++ src/bromine/sonar_experiment_parsing_error.py
@@ -0,0 +1,11 @@
+"""
+This file simulates a Ms Fabric Notebook, stored as a .py file, instead of as a .ipynb one.
+When using the '%run' statement, which is valid for a Jupyter Notebook, but is invalid for a .py file, SonarQube logs a parsing ereror.
+Here we experiment with using NOSONAR to try and suppress the parsing error
+"""
+
+print("Hello")
+
+%run FooBar
+
+print("World")
